8d295e7edd28972782c56a672e80d0ceb135e852
Priya Borad <pp13120@gmail.com>
2025-09-30 12:43:21 +0530

    Update store.js

--- src/Store/store.js
+++ src/Store/store.js
@@ -3,6 +3,8 @@ import productReducer from '../Components/Product'
 
 export const store = configureStore({
     reducer: {
-       
+       products: productReducer,
     },
 })
+
+export default store;
