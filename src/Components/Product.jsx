c8c18534673ce2cc487bb3c5d0655aa87751d450
Priya Borad <pp13120@gmail.com>
2025-09-30 11:54:43 +0530

    first commit

---
+++ src/Components/Product.jsx
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
+
+
+const Product = () => {
+    const products = useSelector((state) => state.products);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Product List</h1>
+        </div>
+    )
+
+}
---
+++ src/Store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from '../Components/Product'
+
+export const store = configureStore({
+    reducer: {
+        product: productReducer,
+    },
+})
